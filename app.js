e5b5691a076e71bec5c9975fb2142c93bb7ff8b4
NamcheolHer <yihs97@naver.com>
2024-09-08 21:05:51 +0900

    백엔드 코드 수정 완료

--- app.js
+++ app.js
@@ -32,5 +32,5 @@ router.get("/", (req, res) => {
 app.use("/api", [router, TodosRouter]);
 
 app.listen(PORT, () => {
-  console.log(PORT, "포트로 서버가 열렸어요!!");
+  console.log(PORT, "포트로 서버가 열렸습니다!!");
 });
